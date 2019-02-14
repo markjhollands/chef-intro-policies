180fd9c1287ba0933904d9c8cfd31b79083f5afc
Mark Hollands <markjhollands@gmail.com>
2019-02-14 20:47:38 +0000

    Initial commit with base file

---
+++ base.rb
@@ -0,0 +1,17 @@
+# base.rb - Describe how you want Chef to build your system.
+#
+# For more information on the Policyfile feature, visit
+# https://github.com/opscode/chef-dk/blob/master/POLICYFILE_README.md
+
+# A name that describes what the system you're building with Chef does.
+name 'base'
+
+# Where to find external cookbooks:
+default_source :supermarket
+
+# Specify a custom source for a cookbook:
+cookbook 'chef-client', git: 'https://github.com/anthonygrees/chef-client.git'
+cookbook 'audit_agr', git: 'https://github.com/anthonygrees/audit_agr.git'
+
+# run_list: chef-client will run these recipes in the order specified.
+run_list 'audit_agr', 'chef-client'
